291aba95ceb60949aa3d92a67ebc94b1883302e3
Yash Bele <109889223+yash-bele@users.noreply.github.com>
2022-12-05 08:34:30 +0530

    commit

--- components/Loading.js
+++ components/Loading.js
@@ -9,7 +9,7 @@ const Loading = () => {
         <link rel='icon' href='' />
       </Head>
       <main className='absolute w-full h-full flex justify-center'>
-        <section className='mt-[168px]>
+        <section className='mt-44>
           <svg
             stroke='currentColor'
             fill='none'
